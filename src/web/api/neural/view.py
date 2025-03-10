5a3994d098fe2fa2a69718da672fbc194ad35047
DaniinXorchenabo <rkbcu@mail.ru>
2025-03-10 19:10:31 +0300

    :sparkles: add neural statistic saving.

--- src/web/api/neural/view.py
+++ src/web/api/neural/view.py
@@ -1,19 +1,25 @@
 import io
-from fastapi import FastAPI, File, UploadFile
+from datetime import datetime
+from time import time
+from typing import Annotated
+from uuid import uuid4
+
+from fastapi import FastAPI, File, UploadFile, Depends
 from PIL import Image
 import torch
 from fastapi.routing import APIRouter
 
 from src.configs.config import CONFIG
+from src.core.db.db_controller import DBController
 from src.core.neural.models_container import MODELS_CONTAINER
-
+from src.web.depends.db_depends import get_db
 
 app = APIRouter(prefix='/neural')
 
 
 # Endpoint для предсказания
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
+async def predict(db_conn: Annotated[DBController, Depends(get_db)], file: UploadFile = File(...),):
     # Читаем содержимое файла
     contents = await file.read()
     # Загружаем изображение через PIL
@@ -26,6 +32,13 @@ async def predict(file: UploadFile = File(...)):
     img_tensor = img_tensor.unsqueeze(0)  # добавляем размер батча
     # Выполняем инференс
     with torch.no_grad():
+        delta_time = time()
         outputs = MODELS_CONTAINER.fashion_mnist_lit_model.model(img_tensor.to(CONFIG.DEVICE))
+        delta_time = time() - delta_time
         predicted_class = torch.argmax(outputs, dim=1).item()
-    return {"predicted_class": predicted_class}
+    db_conn.insert_row('user_logs', row_key=str(_u := uuid4()), data={
+        "datetime": f"{datetime.utcnow().strftime('%Y_%m_%d__%H_%M_%S')}",
+        "result": str(predicted_class),
+        'calc_time': str(delta_time),
+    })
+    return {"predicted_class": predicted_class, "res_id": _u}
