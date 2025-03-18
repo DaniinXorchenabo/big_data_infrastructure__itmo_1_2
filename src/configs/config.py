fbf439e60f6ed49e4eb576e8d30325aec4774ab9
DaniinXorchenabo <rkbcu@mail.ru>
2025-03-18 21:11:28 +0300

    :sparkles: add integrations of secrets with `vault` service.

--- src/configs/config.py
+++ src/configs/config.py
@@ -1,39 +1,50 @@
 import os
+from collections.abc import Callable
 
 import torch
 
-from src.configs.paths_config import ROOT_DIR
+from src.configs import vault
+from src.configs.vault import VAULT_READER, VaultConfig
+from src.utils.logger.logger import logController
 from src.utils.singleton import singleton
 
 
 
 @singleton
 class Config(object):
-    def __init__(self):
+    def __init__(self, vault: VaultConfig, LOGGER):
+        self.LOGGER = LOGGER
         self.DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        self.ROOT_DIR = ROOT_DIR
+        self.variable_source = {
+            'vault': vault.get_vault_secrets,
+            'environ variables': os.environ,
+        }
+
+        self.ROOT_DIR = vault.ROOT_DIR
+        self.LOGS_PATH = vault.LOGS_PATH
 
         self.WEIGHTS_DIR = os.path.join(self.ROOT_DIR, 'neural', 'weights', 'prod')
-        self.MODEL_DIR = os.path.join(self.WEIGHTS_DIR, os.environ.get('AI_WEIGHTS_FILENAME'))
+        self.MODEL_DIR = os.path.join(self.WEIGHTS_DIR, self.load_variable('AI_WEIGHTS_FILENAME'))
 
         self.fashion_mnist_lit_model_weights = os.path.join(
             self.WEIGHTS_DIR,
-            os.environ['AI_WEIGHTS_FILENAME'],
+            self.load_variable('AI_WEIGHTS_FILENAME'),
         )
-        self.BACKEND_PORT = os.environ['BACKEND_PORT']
-        self.BACKEND_HOST_URL = os.environ['BACKEND_HOST_URL']
-        self.BACKEND_MICROSERVICE_DEFAULT_PROTOCOL = os.environ['BACKEND_MICROSERVICE_DEFAULT_PROTOCOL']
-        self.TEST_FROM_NETWORK = self.bool_var(os.environ.get('TEST_FROM_NETWORK', None))
-        self.AI_WEIGHTS_REPO_FILENAME = os.environ['AI_WEIGHTS_REPO_FILENAME']
-        self.AI_WEIGHTS_REPO = os.environ['AI_WEIGHTS_REPO']
+        self.BACKEND_PORT = self.load_variable('BACKEND_PORT')
+        self.BACKEND_HOST_URL = self.load_variable('BACKEND_HOST_URL')
+        self.BACKEND_MICROSERVICE_DEFAULT_PROTOCOL = self.load_variable('BACKEND_MICROSERVICE_DEFAULT_PROTOCOL')
+        self.TEST_FROM_NETWORK = self.bool_var(self.load_variable('TEST_FROM_NETWORK', nullable=True))
+        self.AI_WEIGHTS_REPO_FILENAME = self.load_variable('AI_WEIGHTS_REPO_FILENAME')
+        self.AI_WEIGHTS_REPO = self.load_variable('AI_WEIGHTS_REPO')
 
-        self.DB_LOGIN = os.environ['DB_LOGIN']
-        self.DB_PASSWORD = os.environ['DB_PASSWORD']
-        self.DB_HOST = os.environ['DB_HOST']
-        self.DB_PORT = os.environ['DB_PORT']
+        self.DB_LOGIN = self.load_variable('DB_LOGIN')
+        self.DB_PASSWORD = self.load_variable('DB_PASSWORD')
+        self.DB_HOST = self.load_variable('DB_HOST')
+        self.DB_PORT = self.load_variable('DB_PORT')
         self.DB_CONN_URL = f'http://{self.DB_HOST}:{self.DB_PORT}'
         self._DB_PROPERTIES_FILEPATH = os.path.join(self.ROOT_DIR, 'docker', 'hbase', 'conf', 'hbase-rest-users.properties')
+
         with open(self._DB_PROPERTIES_FILEPATH, 'w') as f:
             print(f'{self.DB_LOGIN}={self.DB_PASSWORD}', file=f)
 
@@ -44,4 +55,25 @@ class Config(object):
             return None
         return str(val).lower().strip() in ['true', '1', 't', 'y', 'yes']
 
-CONFIG = Config()
+    def load_variable(self, name, nullable=False):
+        res = None
+        for k, variable_spase in self.variable_source.items():
+            if isinstance(variable_spase, Callable):
+                res = variable_spase(name)
+            elif name in variable_spase:
+                res = variable_spase[name]
+            if res is not None:
+                self.LOGGER.info(f'Переменная {name} была инициализирована из пространства секретов {k}')
+                return res
+            else:
+                self.LOGGER.info(f'Переменная {name} не была найдена в пространстве секретов {k}')
+        if nullable:
+            return None
+        raise ValueError(f'Переменная {name} не была найдена ни в одном пространстве секретов')
+
+
+LOGGER = None
+with VAULT_READER as vault:
+    with logController(vault) as logger:
+        LOGGER = logger
+        CONFIG = Config(vault, LOGGER)
---
+++ src/configs/vault.py
@@ -0,0 +1,99 @@
+import os
+from datetime import datetime
+
+import hvac
+
+from src.utils.logger.logger import logController
+from src.utils.singleton import singleton
+from src.configs.paths_config import ROOT_DIR
+
+@singleton
+class VaultConfig(object):
+    def __init__(self):
+        self.VAULT_UNSEAL_KEYS = [os.environ.get(f'VAULT_UNSEAL_KEY_{i}') for i in range(10)]
+        self.VAULT_ROOT_TOKEN = os.environ.get(f'VAULT_ROOT_TOKEN')
+        self.VAULT_HOST = os.environ[f'VAULT_HOST']
+        self.VAULT_PORT = 8200
+        self.VAULT_PROTOCOL = 'http'
+        self.VAULT_ADDR = f'{self.VAULT_PROTOCOL}://{self.VAULT_HOST}:{self.VAULT_PORT}'
+        self.VAULT_SECRETS_PATH = os.environ[f'VAULT_SECRETS_PATH']
+        self.VAULT_SECRETS_NAME = os.environ[f'VAULT_SECRETS_NAME']
+        self._vault_client = None
+        self._auth_vault_client = None
+
+        self.ROOT_DIR = ROOT_DIR
+        self.LOGS_PATH = os.path.join(self.ROOT_DIR, 'neural', 'logs', 'prod', f"{(u_:=datetime.utcnow().strftime('%Y_%m_%d__%H_%M_%S'))}.log")
+
+        self.logger = None
+
+
+    def __enter__(self):
+        with logController(self) as logger:
+            self.logger = logger
+            # Инициализация клиента Vault
+            self._vault_client = hvac.Client(url=self.VAULT_ADDR)
+
+            # Проверка состояния хранилища перед разблокировкой
+            if self._vault_client.sys.is_sealed():
+                logger.info("Хранилище запечатано. Начинаем процесс разблокировки...")
+                self.unseal_vault()
+                if not self._vault_client.sys.is_sealed():
+                    logger.info("Хранилище успешно разблокировано.")
+                else:
+                    logger.info("Не удалось полностью разблокировать хранилище. Возможно, требуется больше ключей.")
+            else:
+                logger.info("Хранилище уже разблокировано.")
+
+            return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def get_vault_secrets(self, secret_name):
+        with logController(self) as logger:
+            self.logger = logger
+            if not self.VAULT_ROOT_TOKEN:
+                raise EnvironmentError("Переменная окружения VAULT_ROOT_TOKEN не установлена")
+
+            # Инициализируем клиент Vault (предполагается, что Vault доступен по http://127.0.0.1:8200)
+            self._auth_vault_client = hvac.Client(url=self.VAULT_ADDR, token=self.VAULT_ROOT_TOKEN)
+
+            # Проверяем, что аутентификация прошла успешно
+            if not self._auth_vault_client.is_authenticated():
+                raise Exception("Аутентификация в Vault не удалась")
+
+            # Предположим, что секрет сохранён в kv engine версии 2 по пути secret/my-secret.
+            # В этом примере используется mount_point 'secret' и путь 'my-secret'
+            try:
+                logger.info([self.VAULT_SECRETS_NAME])
+                logger.info([self.VAULT_SECRETS_PATH])
+                logger.info([secret_name])
+                secret_response = self._auth_vault_client.secrets.kv.v2.read_secret_version(
+                    path=f"{self.VAULT_SECRETS_NAME}/{secret_name}",
+                    mount_point=f"{self.VAULT_SECRETS_PATH}"
+                )
+                logger.info(secret_response)
+                secret_data = secret_response['data']['key'].strip()
+                # logger.info("Полученные секреты:")
+                return secret_data
+            except hvac.exceptions.InvalidPath as e:
+                return None
+            except Exception as e:
+                logger.critical("Ошибка при получении секрета:", e)
+                raise e from e
+
+
+    # Функция для разблокировки хранилища
+    def unseal_vault(self):
+        for key in self.VAULT_UNSEAL_KEYS:
+            if key is None:
+                continue
+            if self._vault_client.sys.is_sealed():
+                self._vault_client.sys.submit_unseal_key(key)
+                self.logger.info("Применён unseal-ключ.")
+            else:
+                self.logger.info("Хранилище уже разблокировано.")
+                break
+
+
+VAULT_READER = VaultConfig()
