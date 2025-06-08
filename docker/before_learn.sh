adcc6047317e8b292b142d4b268355d6725a0f49
DaniinXorchenabo <rkbcu@mail.ru>
2025-06-08 22:19:16 +0300

    :wrench: delete jupyter config argument from rus jupyter script.

--- docker/before_learn.sh
+++ docker/before_learn.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 tensorboard --logdir_spec $(python ./docker/jupyter_config/get_tensorboard_logs_files.py)   --host 0.0.0.0 &
-jupyter lab --allow-root --ip=0.0.0.0 --ServerApp.iopub_data_rate_limit=1.0e10 --ServerApp.rate_limit_window=10.0 --config=/root/.jupyter/jupyter_lab_config.py
+jupyter lab --allow-root --ip=0.0.0.0 --ServerApp.iopub_data_rate_limit=1.0e10 --ServerApp.rate_limit_window=10.0
+
+#jupyter lab --allow-root --ip=0.0.0.0 --ServerApp.iopub_data_rate_limit=1.0e10 --ServerApp.rate_limit_window=10.0 --config=/root/.jupyter/jupyter_lab_config.py
