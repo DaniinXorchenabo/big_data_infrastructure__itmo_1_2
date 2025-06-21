1fa22d75ab53baad6c99eee38d946be3aa3ecef4
DaniinXorchenabo <rkbcu@mail.ru>
2025-06-22 02:20:30 +0300

    :closed_lock_with_key: add kubernetes secrets.

--- docker/kubernetes/data-mart-python/service.py
+++ docker/kubernetes/data-mart-python/service.py
@@ -2,18 +2,14 @@ import traceback
 import os
 
 os.environ["HOME"] = "/tmp"
-os.environ["PYSPARK_SUBMIT_ARGS"] = f"""
---conf spark.executor.memory=1G
---conf spark.executor.cores=1
-pyspark-shell
-"""
 os.environ["PYSPARK_PYTHON"] = "/opt/bitnami/python/bin/python3"
 os.environ["PYSPARK_DRIVER_PYTHON"] = "/opt/bitnami/python/bin/python3"
-MONGO_USER = 'admin'
-MONGO_PASS = 'password'
-MONGO_HOST = "host.docker.internal"
-MONGO_PORT = "30717"
-MONGO_ADDR = f"{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}"
+
+os.environ["PYSPARK_SUBMIT_ARGS"] = f"""
+--conf spark.executor.memory={os.environ['K8S_SPARK_EXECUTOR_MEMORY']}
+--conf spark.executor.cores={os.environ['K8S_SPARK_EXECUTOR_CORES']}
+pyspark-shell
+"""
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -22,11 +18,18 @@ from pyspark.sql import SparkSession, DataFrame
 from pyspark.ml.feature import VectorAssembler, StandardScaler
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
+MONGO_USER = os.environ["K8S_MONGO_USER"]
+MONGO_PASS = os.environ["K8S_MONGO_PASSWORD"]
+MONGO_HOST = os.environ["K8S_MONGO_EXTERNAL_HOST"]
+MONGO_PORT = os.environ["K8S_MONGO_EXTERNAL_PORT"]
+MONGO_ADDR = f"{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}"
+
+
 # --- Обработчик фичей ---
 def preprocess(df: DataFrame) -> DataFrame:
     selected = (
         df.select("_id", "product_name", "ingredients_n", "ingredients_sweeteners_n", "scans_n", "additives_n")
-          .na.drop()
+        .na.drop()
     )
     assembler = VectorAssembler(
         inputCols=["ingredients_n", "ingredients_sweeteners_n", "scans_n", "additives_n"],
@@ -39,6 +42,7 @@ def preprocess(df: DataFrame) -> DataFrame:
     ).fit(assembled)
     return scaler.transform(assembled).select("_id", "product_name", "features")
 
+
 # --- Pydantic-модели для JSON-ответа ---
 class Features(BaseModel):
     _id: str
@@ -46,6 +50,7 @@ class Features(BaseModel):
     identify_param: str
     product_name: str
     features: List[float]
+
 
 # Схема чтения из MongoDB
 def get_custom_schema():
@@ -58,31 +63,36 @@ def get_custom_schema():
         StructField("scans_n", IntegerType(), True),
     ])
 
+
 # 1) Инициализация SparkSession
 spark = (
     SparkSession.builder
-        .appName("DataMart-HTTP-Service")
-        .master("spark://host.docker.internal:30077")
-        .config("spark.jars.ivy", "/tmp/.ivy2")
-        .config("spark.executor.instances", "1")
-        .config("spark.executor.cores", "1")
-        .config("spark.executor.memory", "1g")
-        .config("spark.driver.memory", "1g")
-        .config("spark.submit.deployMode", "client")
-        .config("spark.driver.bindAddress", "0.0.0.0")
-        .config("spark.driver.host", "host.docker.internal")
-        .config("spark.driver.port", "31555")
-        .config("spark.blockManager.port", "31556")
-        .config("spark.mongodb.read.connection.uri", f"mongodb://{MONGO_ADDR}")
-        .config("spark.mongodb.write.connection.uri", f"mongodb://{MONGO_ADDR}")
-        .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.3.0,com.redislabs:spark-redis_2.12:3.1.0,org.neo4j:neo4j-connector-apache-spark_2.12:5.3.1_for_spark_3")
-        .config("spark.pyspark.python", "/opt/bitnami/python/bin/python3")
-        .config("spark.executorEnv.PYSPARK_PYTHON", "/opt/bitnami/python/bin/python3")
-        .getOrCreate()
+    .appName("DataMart-HTTP-Service")
+    .master(os.environ['K8S_SPARK_MASTER_URL'])
+    .config("spark.jars.ivy", "/tmp/.ivy2")
+    .config("spark.executor.instances", os.environ['K8S_SPARK_EXECUTOR_INSTANCES'])
+    .config("spark.executor.cores", os.environ['K8S_SPARK_EXECUTOR_CORES'])
+    .config("spark.executor.memory", os.environ['K8S_SPARK_EXECUTOR_MEMORY'])
+
+    .config("spark.submit.deployMode", "client")
+    .config("spark.driver.memory", os.environ['K8S_SPARK_DRIVER_MEMORY'])  # драйверу тоже ограничим RAM, если нужно
+    .config("spark.driver.host", os.environ['K8S_SPARK_DRIVER_HOST'])
+    .config("spark.driver.bindAddress", os.environ['K8S_SPARK_DRIVER_BIND_ADDRESS'])
+    .config("spark.driver.port", os.environ['K8S_SPARK_DRIVER_PORT'])
+    .config("spark.blockManager.port", os.environ['K8S_SPARK_BLOCKMANAGER_PORT'])
+    .config("spark.mongodb.read.connection.uri", f"mongodb://{MONGO_ADDR}")
+    .config("spark.mongodb.write.connection.uri", f"mongodb://{MONGO_ADDR}")
+    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.3.0,"
+                                   "com.redislabs:spark-redis_2.12:3.1.0,"
+                                   "org.neo4j:neo4j-connector-apache-spark_2.12:5.3.1_for_spark_3")
+    .config("spark.pyspark.python", os.environ["PYSPARK_PYTHON"])
+    .config("spark.executorEnv.PYSPARK_PYTHON", os.environ["PYSPARK_DRIVER_PYTHON"])
+    .getOrCreate()
 )
 
 # Инициализируем FastAPI
 app = FastAPI(title="DataMart PyService")
+
 
 @app.get("/preprocessed", response_model=List[Features])
 async def get_preprocessed():
